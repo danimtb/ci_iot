9dab9fb30722306996d273ff4510ea33571668d2
Daniel Manzaneque <danielm@jfrog.com>
2019-05-29 16:49:39 +0200

    deploy with hardocded url

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-REFERENCE="danimtb/Hello/0.1/testing"
 APP_PATH="bin/app"
 
 run_check_directory()
@@ -9,8 +8,8 @@ run_check_directory()
     mkdir check
     cd check
     sleep 1
-    url = "https://dl.bintray.com/danimtb/helloworld/$REFERENCE/0/package/6cc50b139b9c3d27b3e9042d5f5372d327b3a9f7/0/:conan_package.tgz"
-    wget url
+    url="https://dl.bintray.com/danimtb/helloworld/danimtb/Hello/1.0/testing/0/package/90ee443cae5dd5c1b4861766ac14dc6fae231a92/0/conan_package.tgz"
+    wget "$url"
     tar -xzf conan_package.tgz
     cd ..
 }
@@ -24,7 +23,7 @@ read_binary_content ()
 
 
 run_check_directory
-while
+while [ 1 ]
 do
     rm -r execute
     cp -r ./check ./execute
@@ -32,13 +31,13 @@ do
     $APP_PATH &
     app_pid=$!
     sleep 5
-    deploy_content="$(my_function execute)"
+    deploy_content="$(read_binary_content execute)"
     check=1
 
     while [ $check -gt 0 ]
     do
         run_check_directory
-        new_deploy_content="$(my_function check)"
+        new_deploy_content="$(read_binary_content check)"
 
         if [ $deploy_content -ne $new_deploy_content ]; then
             kill -9 $app_pid
